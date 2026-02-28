fc967690186ca8ee55c942836a883841ea7677b7
unknown <jared.hofman@divd.academy>
2026-02-28 21:45:29 +0100

    feat(comp): Navbar start created without handlers and is responsive

---
+++ frontend/src/lib/components/Navbar.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+    import WorksLogo from "../assets/favicon.png";
+    import type { FooterLinks } from "./types";
+
+    const nav_links: FooterLinks[] = [
+        {
+            href: "/",
+            name: "Home",
+        },
+        {
+            href: "#",
+            name: "About",
+        },
+        {
+            href: "#",
+            name: "Services",
+        },
+        {
+            href: "#",
+            name: "Workings",
+        },
+        {
+            href: "#",
+            name: "Register",
+        },
+    ];
+</script>
+
+<header class="w-full h-24 flex justify-center">
+    <nav class="bg-zinc-100 dark:bg-zinc-900 text-neutral-600 dark:text-neutral-400 w-full rounded-4xl m-3 flex items-center 
+        justify-between px-6 shadow-md">
+        <article class="w-auto">
+            <figure class="w-18">
+                <a href="/"><img src={WorksLogo} alt="DIVD.works Logo" /></a>
+            </figure>
+        </article>
+        <article class="w-auto text-3xl font-medium text-black dark:text-white hover:text-neutral-700 
+            dark:hover:text-neutral-300 transition-all block md:hidden">
+            <button aria-label="Open menu">☰</button>
+        </article>
+        <article class="w-auto hidden md:block">
+            {#each nav_links as link}
+                <a href={link.href} class="hover:text-neutral-800 dark:hover:text-neutral-200 ml-8">{link.name}</a>
+            {/each}
+        </article>
+    </nav>
+</header>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,7 +1,11 @@
 <script>
     import Footer from "$lib/components/Footer.svelte";
+    import Navbar from "$lib/components/Navbar.svelte";
 </script>
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
+<Navbar />
+<main>
+    <h1>Welcome to SvelteKit</h1>
+    <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+</main>
 <Footer />
