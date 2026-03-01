11f0159171dafc24b4c67819aa872f57bddd01a6
unknown <jared.hofman@divd.academy>
2026-03-01 11:51:27 +0100

    feat(comp): Navbar created with full functionality and hamburgermenu added

--- frontend/src/lib/components/Navbar.svelte
+++ frontend/src/lib/components/Navbar.svelte
@@ -24,9 +24,12 @@
             name: "Register",
         },
     ];
+
+    let menu_state: boolean = false;
+    const toggle_menu: () => boolean = (): boolean => menu_state = !menu_state;
 </script>
 
-<header class="w-full h-24 flex justify-center">
+<header class="w-full h-24 flex justify-center fixed">
     <nav class="bg-zinc-100 dark:bg-zinc-900 text-neutral-600 dark:text-neutral-400 w-full rounded-4xl m-3 flex items-center 
         justify-between px-6 shadow-md">
         <article class="w-auto">
@@ -36,7 +39,37 @@
         </article>
         <article class="w-auto text-3xl font-medium text-black dark:text-white hover:text-neutral-700 
             dark:hover:text-neutral-300 transition-all block md:hidden">
-            <button aria-label="Open menu">☰</button>
+            <button aria-label="Open menu" on:click={toggle_menu} class="cursor-pointer">☰</button>
+            {#if menu_state}
+                <header class="w-full h-24 flex justify-center fixed inset-0">
+                    <nav class="bg-white dark:bg-black text-neutral-600 dark:text-neutral-400 w-full rounded-4xl m-3 grid 
+                        grid-cols-12 items-start justify-between px-6 shadow-md h-[calc(100dvh-24px)]">
+                        <article class="w-auto col-span-6">
+                            <figure class="w-18">
+                                <a href="/" data-sveltekit-reload><img src={WorksLogo} alt="DIVD.works Logo"/></a>
+                            </figure>
+                        </article>
+                        <article class="w-auto text-black dark:text-white hover:text-neutral-700 dark:hover:text-neutral-300 
+                            transition-all col-span-6 justify-self-end">
+                            <button aria-label="Close menu" on:click={toggle_menu} class="w-auto mt-[18px] -ml-1 
+                                cursor-pointer">
+                                &#x2715;
+                            </button>
+                        </article>
+                        <article class="col-span-12">
+                            {#each nav_links as link}
+                                <a href={link.href} data-sveltekit-reload>
+                                    <div class="bg-zinc-100 dark:bg-zinc-900 hover:bg-zinc-200 dark:hover:bg-zinc-800 
+                                        transition-all rounded-xl hover:text-neutral-800 dark:hover:text-neutral-200 my-8 py-4 
+                                        px-6">
+                                        {link.name}
+                                    </div>
+                                </a>
+                            {/each}
+                        </article>
+                    </nav>
+                </header>
+            {/if}
         </article>
         <article class="w-auto hidden md:block">
             {#each nav_links as link}
