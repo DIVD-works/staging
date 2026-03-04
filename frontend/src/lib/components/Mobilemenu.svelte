981bc61d2a0a4e5d54c8fcfad592d028417071ae
unknown <jared.hofman@divd.academy>
2026-03-04 14:06:59 +0100

    feat(comp): Mobilemenu splitted to its own component and ui improvement + rune mode activated

---
+++ frontend/src/lib/components/Mobilemenu.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+    import { fade } from "svelte/transition";
+    import type { FooterLinks } from "./types";
+    import Icon from "@iconify/svelte";
+
+    const { WorksLogo, nav_links, toggle_menu }: {
+        WorksLogo: string, 
+        nav_links: FooterLinks[], 
+        toggle_menu: () => boolean,
+    } = $props();
+</script>
+
+<header class="w-full h-24 flex justify-center fixed inset-0">
+    <nav class="bg-white dark:bg-black text-neutral-600 dark:text-neutral-400 w-full rounded-4xl m-3 
+        grid grid-cols-12 items-start px-6 shadow-md h-[calc(100dvh-24px)] grid-rows-[auto_1fr]" 
+        transition:fade={{ duration: 300 }}>
+        <article class="w-auto col-span-6">
+            <figure class="w-18">
+                <a href="/" data-sveltekit-reload><img src={WorksLogo} alt="DIVD.works Logo"/></a>
+            </figure>
+        </article>
+        <article class="w-auto text-black dark:text-white hover:text-neutral-700 
+            dark:hover:text-neutral-300 transition-all col-span-6 justify-self-end">
+            <button aria-label="Close menu" onclick={toggle_menu} class="w-auto mt-[18px] -ml-1 
+                cursor-pointer">
+                &#x2715;
+            </button>
+        </article>
+        <article class="col-span-12 my-4">
+            {#each nav_links as link}
+                <a href={link.href} data-sveltekit-reload>
+                    <div class="hover:bg-zinc-200 dark:hover:bg-zinc-800 transition-all rounded-xl 
+                        hover:text-neutral-800 dark:hover:text-neutral-200 my-1 py-3 px-6 flex items-center group">
+                        {link.name.toUpperCase()}
+                        <Icon icon="tabler:circle-arrow-right-filled" font-size="28" class="ml-3 group-hover:ml-5 
+                            transition-all" 
+                        />
+                    </div>
+                </a>
+            {/each}
+        </article>
+    </nav>
+</header>
--- frontend/src/lib/components/Navbar.svelte
+++ frontend/src/lib/components/Navbar.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
     import WorksLogo from "../assets/favicon.png";
+    import Mobilemenu from "./Mobilemenu.svelte";
     import type { FooterLinks } from "./types";
-    import { fade } from "svelte/transition";
 
     const nav_links: FooterLinks[] = [
         {
@@ -34,13 +34,13 @@
         },
     ];
 
-    let menu_state: boolean = false;
+    let menu_state: boolean = $state(false);
     const toggle_menu: () => boolean = (): boolean => menu_state = !menu_state;
 </script>
 
 <header class="w-full h-24 flex justify-center fixed z-10">
-    <nav class="bg-zinc-100 dark:bg-zinc-900 text-neutral-600 dark:text-neutral-400 w-full rounded-4xl m-3 flex items-center 
-        justify-between px-6 shadow-md">
+    <nav class="bg-zinc-100 dark:bg-zinc-900 text-neutral-600 dark:text-neutral-400 w-full rounded-4xl m-3 flex 
+        items-center justify-between px-6 shadow-md">
         <article class="w-auto">
             <figure class="w-18">
                 <a href="/"><img src={WorksLogo} alt="DIVD.works Logo" /></a>
@@ -48,37 +48,9 @@
         </article>
         <article class="w-auto text-3xl font-medium text-black dark:text-white hover:text-neutral-700 
             dark:hover:text-neutral-300 transition-all block md:hidden">
-            <button aria-label="Open menu" on:click={toggle_menu} class="cursor-pointer">☰</button>
+            <button aria-label="Open menu" onclick={toggle_menu} class="cursor-pointer">☰</button>
             {#if menu_state}
-                <header class="w-full h-24 flex justify-center fixed inset-0">
-                    <nav class="bg-white dark:bg-black text-neutral-600 dark:text-neutral-400 w-full rounded-4xl m-3 grid 
-                        grid-cols-12 items-start justify-between px-6 shadow-md h-[calc(100dvh-24px)]" 
-                        transition:fade={{ duration: 300 }}>
-                        <article class="w-auto col-span-6">
-                            <figure class="w-18">
-                                <a href="/" data-sveltekit-reload><img src={WorksLogo} alt="DIVD.works Logo"/></a>
-                            </figure>
-                        </article>
-                        <article class="w-auto text-black dark:text-white hover:text-neutral-700 dark:hover:text-neutral-300 
-                            transition-all col-span-6 justify-self-end">
-                            <button aria-label="Close menu" on:click={toggle_menu} class="w-auto mt-[18px] -ml-1 
-                                cursor-pointer">
-                                &#x2715;
-                            </button>
-                        </article>
-                        <article class="col-span-12">
-                            {#each nav_links as link}
-                                <a href={link.href} data-sveltekit-reload>
-                                    <div class="bg-zinc-100 dark:bg-zinc-900 hover:bg-zinc-200 dark:hover:bg-zinc-800 
-                                        transition-all rounded-xl hover:text-neutral-800 dark:hover:text-neutral-200 my-8 py-4 
-                                        px-6">
-                                        {link.name}
-                                    </div>
-                                </a>
-                            {/each}
-                        </article>
-                    </nav>
-                </header>
+                <Mobilemenu {WorksLogo} {nav_links} {toggle_menu} />
             {/if}
         </article>
         <article class="w-auto hidden md:block">
