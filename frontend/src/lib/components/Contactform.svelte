e05d2e1ddb619d71d1a6dbb09ba6f5495d707056
unknown <jared.hofman@divd.academy>
2026-03-09 02:16:25 +0100

    refactor(form): Removed connection to JotForm and boilerplate code/cookies for Formspree

--- frontend/src/lib/components/Contactform.svelte
+++ frontend/src/lib/components/Contactform.svelte
@@ -62,54 +62,26 @@
     });
 </script>
 
-<form id="253242909733056" method="post" action="https://eu-submit.jotform.com/submit/253242909733056" onsubmit={
+<form method="post" action="https://formspree.io/f/mpqyqzvr" onsubmit={
     (event: SubmitEvent): boolean | void => !form.agreement.check && event?.preventDefault()
-} name="form_253242909733056" accept-charset="utf-8" autocomplete="on" novalidate={true}>
+} accept-charset="utf-8" autocomplete="on">
     <fieldset class="border-2 border-purple-500 rounded-2xl grid grid-cols-12 w-full gap-3 font-medium text-base sm:text-lg 
         p-4 sm:p-6">
         <legend class="text-black dark:text-white text-center text-xl sm:text-2xl">Contact information</legend>
 
-        <!-- Start hidden JotForm input validators -->
-        <input type="hidden" class="input_default col-span-12 caret-black! dark:caret-white!" name="formID" 
-            value="253242909733056" 
-        />
-        <input id="JWTContainer" type="hidden" class="input_default col-span-12 caret-black! dark:caret-white!" value="" />
-        <input id="cardinalOrderNumber" type="hidden" class="input_default col-span-12 caret-black! dark:caret-white!" 
-            value="" 
-        />
-        <input id="jsExecutionTracker" type="hidden" class="input_default col-span-12 caret-black! dark:caret-white!" 
-            name="jsExecutionTracker" value="build-date-1772990272965" 
-        />
-        <input id="submitSource" type="hidden" class="input_default col-span-12 caret-black! dark:caret-white!" 
-            name="submitSource" value="unknown" 
-        />
-        <input id="submitDate" type="hidden" class="input_default col-span-12 caret-black! dark:caret-white!" 
-            name="submitDate" value="undefined" 
-        />
-        <input id="buildDate" type="hidden" class="input_default col-span-12 caret-black! dark:caret-white!" 
-            name="buildDate" value="1772990272965" 
-        />
-        <input type="hidden" class="input_default col-span-12 caret-black! dark:caret-white!" name="uploadServerUrl" 
-            value="https://upload.jotform.com/upload" 
-        />
-        <input type="hidden" class="input_default col-span-12 caret-black! dark:caret-white!" name="eventObserver" 
-            value="1" 
-        />
-        <!-- End hidden JotForm input validators -->
-
         <p id="full_name" class="label_default">Full Name: </p>
         <div class="form_div md:col-span-6!">
             <input id="first_name" type="text" class="input_default mb-2" placeholder="Enter your First Name" 
-                aria-describedby="full_name first_name" name="q2_q2_fullname0[first]" bind:value={form.firstname.field} 
-                onblur={(): boolean => form.firstname.noerror = true}
+                aria-describedby="full_name first_name" name="firstname" bind:value={form.firstname.field} 
+                onblur={(): boolean => form.firstname.noerror = true} required minlength="2" maxlength="100"
             />
             <small class="mb-0 md:mb-3">First Name</small>
         </div>
 
         <div class="form_div md:col-span-6!">
             <input id="last_name" type="text" class="input_default mb-2" placeholder="Enter your Last Name" 
-                aria-describedby="full_name last_name" name="q2_q2_fullname0[last]" bind:value={form.lastname.field} 
-                onblur={(): boolean => form.lastname.noerror = true}
+                aria-describedby="full_name last_name" name="lastname" bind:value={form.lastname.field} 
+                onblur={(): boolean => form.lastname.noerror = true} required minlength="2" maxlength="100"
             />
             <small class="mb-3">Last Name</small>
         </div>
@@ -122,8 +94,9 @@
 
         <label for="company" class="label_default after:text-black! dark:after:text-white!">Company: </label>
         <input id="company" type="text" class="input_default col-span-12 caret-black! dark:caret-white!" 
-            placeholder="Enter your Company" name="q3_q3_textbox1" bind:value={form.company.field} 
+            placeholder="Enter your Company" name="company" bind:value={form.company.field} 
             onblur={(): boolean => form.company.field.length <= 0 ? form.company.noerror = false : form.company.noerror = true}
+            minlength="2" maxlength="100"
         />
         {#if !form.company.check && form.company.noerror}
             <div class="error_default"><p class="error">{form.company.errormessage}</p></div>
@@ -131,8 +104,9 @@
 
         <label for="job_title" class="label_default after:text-black! dark:after:text-white!">Job Title: </label>
         <input id="job_title" type="text" class="input_default col-span-12 caret-black! dark:caret-white!" 
-            placeholder="Enter your Job Title" name="q4_q4_textbox2" bind:value={form.jobtitle.field} onblur={(): boolean => 
+            placeholder="Enter your Job Title" name="jobtitle" bind:value={form.jobtitle.field} onblur={(): boolean => 
             form.jobtitle.field.length <= 0 ? form.jobtitle.noerror = false : form.jobtitle.noerror = true}
+            minlength="2" maxlength="100"
         />
         {#if !form.jobtitle.check && form.jobtitle.noerror}
             <div class="error_default"><p class="error">{form.jobtitle.errormessage}</p></div>
@@ -141,8 +115,8 @@
         <label for="email_address" class="label_default">E-mail Address: </label>
         <div class="form_div">
             <input id="email_address" type="email" class="input_default col-span-12 mb-2" 
-                placeholder="Enter your E-mail Address" name="q5_q5_email3" bind:value={form.emailaddress.field} 
-                onblur={(): boolean => form.emailaddress.noerror = true}
+                placeholder="Enter your E-mail Address" name="email" bind:value={form.emailaddress.field} 
+                onblur={(): boolean => form.emailaddress.noerror = true} required minlength="6" maxlength="250"
             />
             <small class="mb-3">example@example.com</small>
         </div>
@@ -153,10 +127,10 @@
         <label for="phone_number" class="label_default after:text-black! dark:after:text-white!">Phone Number: </label>
         <div class="form_div">
             <input id="phone_number" type="tel" class="input_default col-span-12 caret-black! dark:caret-white!" 
-                placeholder="Enter your Phone Number" name="q6_q6_phone4[full]" bind:value={form.phonenumber.field} 
+                placeholder="Enter your Phone Number" name="phonenumber" bind:value={form.phonenumber.field} 
                 onblur={(): boolean => form.phonenumber.field.toString().length <= 0 ? 
                     form.phonenumber.noerror = false : form.phonenumber.noerror = true
-                }
+                } minlength="10" maxlength="10"
             />
             <small class="mb-3">1234567890</small>
         </div>
@@ -166,16 +140,16 @@
 
         <label for="your_message" class="label_default">Your Message: </label>
         <textarea id="your_message" class="input_default h-48! col-span-12 py-3" placeholder="Enter your Message" 
-            name="q7_q7_textarea5" bind:value={form.yourmessage.field} onblur={(): boolean => form.yourmessage.noerror = true}>
-        </textarea>
+            name="yourmessage" bind:value={form.yourmessage.field} onblur={(): boolean => form.yourmessage.noerror = true}
+            required minlength="50" maxlength="2500"></textarea>
         {#if !form.yourmessage.check && form.yourmessage.noerror}
             <div class="error_default"><p class="error">{form.yourmessage.errormessage}</p></div>
         {/if}
 
         <div class="form_div flex-row!">
             <input id="agreement" type="checkbox" class="border border-black dark:border-white w-8 h-8 rounded-md 
-                text-purple-500" name="checkbox" bind:checked={form.agreement.field} 
-                onblur={(): boolean => form.agreement.noerror = true}
+                text-purple-500" name="agreement" bind:checked={form.agreement.field} 
+                onblur={(): boolean => form.agreement.noerror = true} required
             />
             <label for="agreement" class="label_default col-auto! pl-3 mb-3">
                 By submitting this form, you agree to the use of your information to fulfill your request and to be 
@@ -187,24 +161,12 @@
             <div class="error_default"><p class="error">{form.agreement.errormessage}</p></div>
         {/if}
 
-        <!-- Start hidden JotForm input validators -->
-        <input id="input_8" type="hidden" class="input_default col-span-12 caret-black! dark:caret-white!" 
-            name="q8_q8_widget_TermsAndConditions6" value="" 
-        />
         <span class="hidden col-span-12">
             Should be Empty: 
-            <input type="text" class="input_default col-span-12 caret-black! dark:caret-white!" name="website" 
+            <input type="text" class="input_default col-span-12 caret-black! dark:caret-white!" name="emptyfield" 
                 bind:value={form.honeypot.field} onblur={(): boolean => form.honeypot.noerror = true} 
             />
         </span>
-        <input id="simple_spc" type="hidden" class="input_default col-span-12 caret-black! dark:caret-white!" 
-            name="simple_spc" value="253242909733056"
-        />
-        <input id="widget_settings_8" type="hidden" class="input_default col-span-12 caret-black! dark:caret-white!" 
-            data-version="2" 
-            value="%5B%7B%22name%22%3A%22termsText%22%2C%22value%22%3A%22By%20submitting%20this%20form%2C%20you%20agree%20to%20the%20use%20of%20your%20information%20to%20fulfill%20your%20request%20and%20to%20be%20contacted%20about%20other%20relevant%20content%2C%20services%2C%20and%20events.%20For%20more%20information%2C%20please%20see%20our%20Privacy%20Notice.%22%7D%2C%7B%22name%22%3A%22termsLink%22%2C%22value%22%3A%22https%3A%2F%2Fwww.divd.works%2Fabout%2Fprivacy%22%7D%2C%7B%22name%22%3A%22theme%22%2C%22value%22%3A%22default%22%7D%2C%7B%22name%22%3A%22acceptedText%22%2C%22value%22%3A%22Accepted%22%7D%5D" 
-        />
-        <!-- End hidden JotForm input validators -->
 
         {#if form.agreement.check}
             <div class="col-span-12 grid grid-cols-12 w-full gap-5 text-white font-medium text-lg">
